368202712ccaa73c154b9eeca2de28518785f212
Tiuw <danendraalvyn@gmail.com>
2024-12-11 11:58:36 +0700

    Score is Saved

--- app/src/main/java/com/capstone/bindetective/ui/quiz/QuizFragment.kt
+++ app/src/main/java/com/capstone/bindetective/ui/quiz/QuizFragment.kt
@@ -1,6 +1,5 @@
 package com.capstone.bindetective.ui.quiz
 
-import QuizAdapter
 import android.os.Bundle
 import android.util.Log
 import android.view.LayoutInflater
@@ -14,67 +13,52 @@ import androidx.recyclerview.widget.RecyclerView
 import com.capstone.bindetective.R
 import com.capstone.bindetective.model.QuizResponseItem
 import com.capstone.bindetective.ui.quizdetail.QuizDetailFragment
-import com.capstone.bindetective.ui.quiz.QuizViewModel
 
 class QuizFragment : Fragment() {
 
     private lateinit var quizViewModel: QuizViewModel
     private lateinit var recyclerView: RecyclerView
 
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        Log.d("QuizFragment", "onCreateView called")
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         return inflater.inflate(R.layout.fragment_quiz, container, false)
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        Log.d("QuizFragment", "onViewCreated called")
-
         recyclerView = view.findViewById(R.id.recyclerView)
-
         quizViewModel = ViewModelProvider(this).get(QuizViewModel::class.java)
 
         recyclerView.layoutManager = LinearLayoutManager(requireContext())
 
-        // Retrieve the score passed from QuizResultFragment
-        val score = arguments?.getInt("score")
-
+        // Observe the quizzes data
         quizViewModel.quizzes.observe(viewLifecycleOwner) { quizzes ->
-            Log.d("QuizFragment", "Received quizzes: $quizzes")
+            val savedScore = quizViewModel.getScore()  // Retrieve the saved score
+            recyclerView.adapter = QuizAdapter(quizzes, savedScore) { quizItem ->
+                Log.d("QuizFragment", "Quiz item clicked: ${quizItem.quizId}")
 
-            if (quizzes != null && quizzes.isNotEmpty()) {
-                Log.d("QuizFragment", "Setting adapter with quiz items")
+                val fragmentTransaction = requireActivity().supportFragmentManager.beginTransaction()
+                val quizDetailFragment = QuizDetailFragment()
 
-                // Pass quizzes and score to the adapter
-                recyclerView.adapter = QuizAdapter(quizzes, score) { quiz ->
-                    Log.d("QuizFragment", "Quiz item clicked: ${quiz.quizId}")
-
-                    val fragmentTransaction = requireActivity().supportFragmentManager.beginTransaction()
-                    val quizDetailFragment = QuizDetailFragment()
-
-                    val bundle = Bundle().apply {
-                        putString("quizId", quiz.quizId)
-                    }
-                    quizDetailFragment.arguments = bundle
-
-                    fragmentTransaction.replace(R.id.fragment_container, quizDetailFragment)
-                    fragmentTransaction.addToBackStack(null)
-                    fragmentTransaction.commit()
-
-                    Log.d("QuizFragment", "Navigated to QuizDetailFragment manually")
+                val bundle = Bundle().apply {
+                    putString("quizId", quizItem.quizId)
                 }
-            } else {
-                Log.d("QuizFragment", "No quizzes available")
+                quizDetailFragment.arguments = bundle
+
+                fragmentTransaction.replace(R.id.fragment_container, quizDetailFragment)
+                fragmentTransaction.addToBackStack(null)
+                fragmentTransaction.commit()
             }
         }
 
-        quizViewModel.error.observe(viewLifecycleOwner) { errorMessage ->
-            Log.e("QuizFragment", "Error fetching quizzes: $errorMessage")
+        quizViewModel.error.observe(viewLifecycleOwner) { errorMsg ->
+            Log.e("QuizFragment", "API Error: $errorMsg")
         }
 
-        quizViewModel.fetchQuizzes()
-        Log.d("QuizFragment", "Initiating quizViewModel.fetchQuizzes() call")
+        quizViewModel.score.observe(viewLifecycleOwner) { score ->
+            if (score != null) {
+                Log.d("QuizFragment", "User's score: $score")
+            }
+        }
+
+        quizViewModel.fetchQuizzes()  // Fetch quiz data
     }
 }
